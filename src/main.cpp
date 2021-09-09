054b0a9979aa1ce0e611e0cd7ccf1f57c6280ceb
KacRuz <kacper.ruzczynski@tlen.pl>
2021-09-09 14:52:57 +0200

    copy code and build folder

--- src/main.cpp
+++ src/main.cpp
@@ -1,2 +1,31 @@
 #include <iostream>
 #include <ncurses.h>
+#include <string>
+
+int main(){
+    initscr();
+    noecho();
+    raw();
+    start_color();
+    curs_set(0);
+    init_pair(1, COLOR_RED, COLOR_BLACK);
+
+    keypad(stdscr, TRUE);
+
+    int row,col;
+    getmaxyx(stdscr,row,col);
+    char ch = 'b';
+    attron(A_STANDOUT);
+    attron(COLOR_PAIR(1));
+    for(int y=0; y<row; y++){
+        for(int x=0; x<col; x++){
+            mvprintw(y,x,"%c",ch);
+        }
+    }
+    attroff(COLOR_PAIR(1));
+    mvaddch(0,0,ACS_STERLING);
+    printw("DSADASD");
+    getch();
+    endwin();
+    return 0;
+}
